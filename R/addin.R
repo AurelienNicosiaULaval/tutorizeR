26e7f1d785bab0bb5e8867498429a8e629bd22e3
Aurélien Nicosia <116816597+AurelienNicosiaULaval@users.noreply.github.com>
2025-12-06 16:08:51 -0500

    feat: Add new RStudio addin to convert a folder of Rmd/qmd files to tutorials.

--- R/addin.R
+++ R/addin.R
@@ -13,3 +13,20 @@ launch_tutorizeR_addin <- function() {
 
     convert_to_tutorial(path)
 }
+
+#' RStudio Addin: Convert Folder to Tutorial
+#'
+#' Interactively selects a folder and converts all contained .Rmd/.qmd files.
+#' @export
+#' @importFrom rstudioapi selectDirectory
+launch_tutorizeR_folder_addin <- function() {
+    folder <- rstudioapi::selectDirectory(
+        caption = "Select Folder to Convert"
+    )
+
+    if (is.null(folder) || folder == "") {
+        return(invisible(NULL))
+    }
+
+    convert_folder(folder)
+}
